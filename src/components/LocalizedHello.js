c7d85a7e79c0b9d3dcfc911cd11306d7856a77c1
dgmana <mana@diffgaming.com>
2026-07-23 14:51:18 +0700

    Improve localized greeting fallbacks

--- src/components/LocalizedHello.js
+++ src/components/LocalizedHello.js
@@ -100,11 +100,13 @@ const languagesByCountry = {
   AL: "sq",
   AM: "hy",
   AO: "pt",
+  AQ: "en",
   AR: "es",
   AS: "en",
   AT: "de",
   AU: "en",
   AW: "nl",
+  AX: "sv",
   AZ: "az",
   BA: "bs",
   BB: "en",
@@ -115,16 +117,20 @@ const languagesByCountry = {
   BH: "ar",
   BI: "fr",
   BJ: "fr",
+  BL: "fr",
   BM: "en",
   BN: "ms",
   BO: "es",
+  BQ: "nl",
   BR: "pt",
   BS: "en",
   BT: "dz",
+  BV: "no",
   BW: "en",
   BY: "be",
   BZ: "en",
   CA: "en",
+  CC: "en",
   CD: "fr",
   CF: "fr",
   CG: "fr",
@@ -138,6 +144,8 @@ const languagesByCountry = {
   CR: "es",
   CU: "es",
   CV: "pt",
+  CW: "nl",
+  CX: "en",
   CY: "el",
   CZ: "cs",
   DE: "de",
@@ -149,6 +157,7 @@ const languagesByCountry = {
   EC: "es",
   EE: "et",
   EG: "ar",
+  EH: "ar",
   ER: "ar",
   ES: "es",
   ET: "am",
@@ -172,11 +181,13 @@ const languagesByCountry = {
   GP: "fr",
   GQ: "es",
   GR: "el",
+  GS: "en",
   GT: "es",
   GU: "en",
   GW: "pt",
   GY: "en",
   HK: "zh",
+  HM: "en",
   HN: "es",
   HR: "hr",
   HT: "fr",
@@ -186,6 +197,7 @@ const languagesByCountry = {
   IL: "he",
   IM: "en",
   IN: "hi",
+  IO: "en",
   IQ: "ar",
   IR: "fa",
   IS: "is",
@@ -248,6 +260,7 @@ const languagesByCountry = {
   NO: "no",
   NP: "ne",
   NR: "en",
+  NU: "en",
   NZ: "en",
   OM: "ar",
   PA: "es",
@@ -258,6 +271,7 @@ const languagesByCountry = {
   PK: "ur",
   PL: "pl",
   PM: "fr",
+  PN: "en",
   PR: "es",
   PS: "ar",
   PT: "pt",
@@ -277,6 +291,7 @@ const languagesByCountry = {
   SG: "en",
   SH: "en",
   SI: "sl",
+  SJ: "no",
   SK: "sk",
   SL: "en",
   SM: "it",
@@ -286,6 +301,7 @@ const languagesByCountry = {
   SS: "en",
   ST: "pt",
   SV: "es",
+  SX: "nl",
   SY: "ar",
   SZ: "en",
   TC: "en",
@@ -298,6 +314,7 @@ const languagesByCountry = {
   TM: "tk",
   TN: "ar",
   TO: "en",
+  TF: "fr",
   TR: "tr",
   TT: "en",
   TV: "en",
@@ -305,6 +322,7 @@ const languagesByCountry = {
   TZ: "sw",
   UA: "uk",
   UG: "en",
+  UM: "en",
   US: "en",
   UY: "es",
   UZ: "uz",
@@ -315,6 +333,7 @@ const languagesByCountry = {
   VI: "en",
   VN: "vi",
   VU: "en",
+  WF: "fr",
   WS: "en",
   XK: "sq",
   YE: "ar",
@@ -329,13 +348,239 @@ function greetingFromLocale(locale) {
   return greetingsByLanguage[language];
 }
 
+function normalizeCountryName(value) {
+  return value
+    ?.toString()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, " ")
+    .trim();
+}
+
+const countryNameAliases = {
+  "aland islands": "AX",
+  "antigua": "AG",
+  "antigua and barbuda": "AG",
+  "bahamas": "BS",
+  "bolivia": "BO",
+  "bosnia": "BA",
+  "bosnia and herzegovina": "BA",
+  "bosnia herzegovina": "BA",
+  "british virgin islands": "VG",
+  "brunei": "BN",
+  "burma": "MM",
+  "cabo verde": "CV",
+  "cape verde": "CV",
+  "caribbean netherlands": "BQ",
+  "central african republic": "CF",
+  "cocos islands": "CC",
+  "cocos keeling islands": "CC",
+  "congo brazzaville": "CG",
+  "congo kinshasa": "CD",
+  "cote d ivoire": "CI",
+  "cote divoire": "CI",
+  "curacao": "CW",
+  "czech republic": "CZ",
+  "czechia": "CZ",
+  "democratic republic of the congo": "CD",
+  "dominican republic": "DO",
+  "dr congo": "CD",
+  "east timor": "TL",
+  "eswatini": "SZ",
+  "falkland islands": "FK",
+  "federated states of micronesia": "FM",
+  "faroe islands": "FO",
+  "french southern territories": "TF",
+  "hong kong": "HK",
+  "iran": "IR",
+  "ivory coast": "CI",
+  "laos": "LA",
+  "macedonia": "MK",
+  "macao": "MO",
+  "macau": "MO",
+  "marshall islands": "MH",
+  "micronesia": "FM",
+  "moldova": "MD",
+  "myanmar": "MM",
+  "myanmar burma": "MM",
+  "netherlands antilles": "BQ",
+  "palau": "PW",
+  "palestine": "PS",
+  "pitcairn": "PN",
+  "reunion": "RE",
+  "republic of the congo": "CG",
+  "russia": "RU",
+  "saint barthelemy": "BL",
+  "saint helena": "SH",
+  "saint kitts": "KN",
+  "saint kitts and nevis": "KN",
+  "saint lucia": "LC",
+  "saint martin": "MF",
+  "saint pierre": "PM",
+  "saint pierre and miquelon": "PM",
+  "saint vincent": "VC",
+  "saint vincent and the grenadines": "VC",
+  "sao tome": "ST",
+  "sao tome and principe": "ST",
+  "slovak republic": "SK",
+  "south korea": "KR",
+  "north korea": "KP",
+  "south sudan": "SS",
+  "st barthelemy": "BL",
+  "st helena": "SH",
+  "st kitts": "KN",
+  "st kitts and nevis": "KN",
+  "st lucia": "LC",
+  "st martin": "MF",
+  "st pierre": "PM",
+  "st pierre and miquelon": "PM",
+  "st vincent": "VC",
+  "st vincent and the grenadines": "VC",
+  "svalbard": "SJ",
+  "svalbard and jan mayen": "SJ",
+  "syria": "SY",
+  "taiwan": "TW",
+  "tanzania": "TZ",
+  "the bahamas": "BS",
+  "the gambia": "GM",
+  "timor leste": "TL",
+  "trinidad": "TT",
+  "trinidad and tobago": "TT",
+  "turkey": "TR",
+  "turkiye": "TR",
+  "turks and caicos": "TC",
+  "turks and caicos islands": "TC",
+  "uk": "GB",
+  "united arab emirates": "AE",
+  "united kingdom of great britain and northern ireland": "GB",
+  "united kingdom": "GB",
+  "united states minor outlying islands": "UM",
+  "united states": "US",
+  "united states of america": "US",
+  "us virgin islands": "VI",
+  "usa": "US",
+  "vatican": "VA",
+  "vatican city": "VA",
+  "venezuela": "VE",
+  "viet nam": "VN",
+  "vietnam": "VN",
+  "wallis and futuna": "WF",
+  "western sahara": "EH"
+};
+
+function countryCodeFromName(countryName) {
+  const normalizedName = normalizeCountryName(countryName);
+
+  if (!normalizedName) return null;
+  if (countryNameAliases[normalizedName]) return countryNameAliases[normalizedName];
+
+  if (typeof Intl === "undefined" || typeof Intl.DisplayNames === "undefined") {
+    return null;
+  }
+
+  const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+  return Object.keys(languagesByCountry).find((countryCode) => (
+    normalizeCountryName(regionNames.of(countryCode)) === normalizedName
+  ));
+}
+
 function greetingFromCountryCode(countryCode) {
   const language = languagesByCountry[countryCode?.toUpperCase()];
   return greetingsByLanguage[language];
 }
 
+function greetingFromCountry(countryCode, countryName) {
+  return greetingFromCountryCode(countryCode) || greetingFromCountryCode(countryCodeFromName(countryName));
+}
+
+const lookupServices = [
+  {
+    url: "https://ipapi.co/json/",
+    countryCode(data) {
+      return data?.country_code || data?.country;
+    },
+    countryName(data) {
+      return data?.country_name;
+    }
+  },
+  {
+    url: "https://ipwho.is/",
+    countryCode(data) {
+      if (data?.success === false) return null;
+      return data?.country_code;
+    },
+    countryName(data) {
+      if (data?.success === false) return null;
+      return data?.country;
+    }
+  },
+  {
+    url: "https://freeipapi.com/api/json/",
+    countryCode(data) {
+      return data?.countryCode;
+    },
+    countryName(data) {
+      return data?.countryName;
+    }
+  }
+];
+
 function firstGreetingFromLocales(locales) {
   return locales.map(greetingFromLocale).find(Boolean);
+}
+
+async function fetchJsonWithTimeout(url, signal) {
+  const timeoutController = new AbortController();
+  const timeout = window.setTimeout(() => timeoutController.abort(), 3500);
+
+  function abortTimeout() {
+    timeoutController.abort();
+  }
+
+  signal?.addEventListener("abort", abortTimeout, { once: true });
+
+  try {
+    const response = await fetch(url, {
+      cache: "no-store",
+      signal: timeoutController.signal
+    });
+
+    if (!response.ok) {
+      throw new Error("IP lookup unavailable");
+    }
+
+    return response.json();
+  } finally {
+    window.clearTimeout(timeout);
+    signal?.removeEventListener("abort", abortTimeout);
+  }
+}
+
+async function lookupGreeting(signal) {
+  const languageGreeting = firstGreetingFromLocales(navigator.languages || [navigator.language]);
+
+  for (const service of lookupServices) {
+    try {
+      const data = await fetchJsonWithTimeout(service.url, signal);
+      const countryGreeting = greetingFromCountry(service.countryCode(data), service.countryName(data));
+
+      if (countryGreeting) {
+        return countryGreeting;
+      }
+
+      const serviceLanguageGreeting = firstGreetingFromLocales(data?.languages?.split(",") || []);
+      if (serviceLanguageGreeting) {
+        return serviceLanguageGreeting;
+      }
+    } catch (error) {
+      if (signal?.aborted) {
+        throw error;
+      }
+    }
+  }
+
+  return languageGreeting || greetingsByLanguage.en;
 }
 
 function greetingScale(value) {
@@ -353,32 +598,20 @@ export default function LocalizedHello() {
   const [greeting, setGreeting] = useState(greetingsByLanguage.en);
 
   useEffect(() => {
-    let cancelled = false;
+    const controller = new AbortController();
 
-    fetch("https://ipapi.co/json/")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("IP lookup unavailable");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (cancelled) {
-          return;
-        }
-
-        const languageGreeting = firstGreetingFromLocales(data?.languages?.split(",") || []);
-        const countryGreeting = greetingFromCountryCode(data?.country_code);
-        setGreeting(countryGreeting || languageGreeting || greetingsByLanguage.en);
+    lookupGreeting(controller.signal)
+      .then((resolvedGreeting) => {
+        setGreeting(resolvedGreeting);
       })
       .catch(() => {
-        if (!cancelled) {
+        if (!controller.signal.aborted) {
           setGreeting(greetingsByLanguage.en);
         }
       });
 
     return () => {
-      cancelled = true;
+      controller.abort();
     };
   }, []);
 
